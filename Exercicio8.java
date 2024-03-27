d975e87859533b530a938b49885e19fce64c290c
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-27 10:23:32 -0300

    oitavo commit

---
+++ Exercicio8.java
@@ -0,0 +1,13 @@
+public class Exercicio8 {
+    public static void Executar(){
+        int numero = Prompt.lerInteiro("Digite um número: ");
+        
+        if (numero >= 50) {
+            System.out.println("O número é maior ou igual a 50.");
+        } else {
+            System.out.println("O número é menor que 50.");
+        }
+    }
+}
+    
+
